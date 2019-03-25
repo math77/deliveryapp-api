1f3ba9b2c2df2d07b2a05806ebd8f283a66a0631
Matheus Emanuel <matheusemanuel745@gmail.com>
2019-03-24 21:00:40 -0300

    Primeiros models criados

--- deliveryapp/api/models.py
+++ deliveryapp/api/models.py
@@ -1,3 +1,85 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 
-# Create your models here.
+from .managers import CustomUserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(_('username'), max_length=30, unique=True)
+    first_name = models.CharField(_('First Name'), max_length=30, null=False, blank=False)
+    last_name = models.CharField(_('Last Name'), max_length=30, null=False, blank=False)
+    email = models.EmailField(_('email address'), unique=True)
+    cpf = models.CharField(max_length=11, null=False, blank=False, unique=True)
+    phone = models.CharField(max_length=13, null=False, blank=False)
+    about = models.CharField(max_length=150)
+    criminal_background_check_pic = models.ImageField(upload_to='pic_folder/')
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(null=False, auto_now_add=True)
+    profile_pic = models.ImageField(upload_to='pic_profile_folder/')
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'cpf', 'phone', 'username']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+
+class Payment(models.Model):
+
+    token = models.CharField(max_length=80, null=False, blank=False)
+    confirmed = models.BooleanField(default=False)
+    confirmation_date = models.DateTimeField()
+
+
+class TypeLocomotion(models.Model):
+
+    description = models.CharField(max_length=30, null=False, blank=False)
+
+
+
+class Trip(models.Model):
+
+    completed = models.BooleanField(default=False)
+    created = models.DateTimeField(null=False, auto_now_add=True)
+    id_type_locomotion = models.ForeignKey(TypeLocomotion, on_delete=models.DO_NOTHING)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+
+class Delivery(models.Model):
+
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created = models.DateTimeField(null=False, auto_now_add=True)
+    id_payment = models.OneToOneField(Payment, on_delete=models.CASCADE)
+    id_trip = models.ForeignKey(Trip, on_delete=models.DO_NOTHING)
+
+
+class Order(models.Model):
+
+    initial_latitude = models.FloatField(null=False)
+    initial_longitude = models.FloatField(null=False)
+    final_latitude = models.FloatField(null=False)
+    final_longitude = models.FloatField(null=False)
+    created = models.DateTimeField(null=False, auto_now_add=True)
+    delivered = models.BooleanField(default=False)
+    id_delivery = models.ForeignKey(Delivery, on_delete=models.DO_NOTHING)
+    id_user_sender = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+
+
+class Rating(models.Model):
+
+    rating = models.IntegerField()
+    comment = models.TextField()
+    id_user_evaluated = models.ForeignKey(User, related_name='user_evaluated', on_delete=models.DO_NOTHING)
+    id_user_appraiser = models.ForeignKey(User, related_name='user_appraiser', on_delete=models.DO_NOTHING)
+
+
+objects = models.Manager()
--- deliveryapp/deliveryapp/settings.py
+++ deliveryapp/deliveryapp/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'api.User'
 
 # Application definition
 
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'deliveryapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DeliveryAppDB',
+        'USER': 'admin_deliveryapp',
+        'PASSWORD': '$#@!-delivery4pp',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
