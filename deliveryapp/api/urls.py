408c5fca271451ba73cf32ab4c87971b0722ac3b
Matheus Emanuel <matheusemanuel745@gmail.com>
2019-03-30 01:31:51 -0300

    Criada as urls para as views de Order, Trip, Payment e Rating

--- deliveryapp/api/urls.py
+++ deliveryapp/api/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('login/', obtain_jwt_token),
     url(r'^users/$', views.UserList.as_view(), name='user-list'),
     url(r'^deliverys/$', views.DeliveryList.as_view(), name='delivery-list'),
+    url(r'^orders/$', views.OrderList.as_view(), name='order-list'),
+    url(r'^trips/$', views.TripList.as_view(), name='trip-list'),
+    url(r'^payments/$', views.PaymentList.as_view(), name='payment-list'),
+    url(r'^ratings/$', views.RatingList.as_view(), name='rating-list'),
 ]
