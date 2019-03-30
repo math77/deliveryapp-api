94c7288b39267b7821051d14209cf18cd263e333
Matheus Emanuel <matheusemanuel745@gmail.com>
2019-03-30 01:28:53 -0300

    Criado serializers e views   - Criado serializers para os models de Payment, Order, Trip, Rating e TypeLocomotion   - Criada views(listar e criar) para Payment, Order, Trip e Rating

--- deliveryapp/api/serializers.py
+++ deliveryapp/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import User, Delivery
+from .models import User, Delivery, Order, Rating, Trip, Payment, TypeLocomotion
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -14,4 +14,39 @@ class DeliverySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Delivery
+        fields = '__all__'
+
+
+class OrderSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+
+class RatingSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Rating
+        fields = '__all__'
+
+
+class TripSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Trip
+        fields = '__all__'
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Payment
+        fields = '__all__'
+
+
+class TypeLocomotionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = TypeLocomotion
         fields = '__all__'
--- deliveryapp/api/views.py
+++ deliveryapp/api/views.py
@@ -1,18 +1,43 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from .models import User, Delivery
-from .serializers import UserSerializer, DeliverySerializer
+from . import models
+from . import serializers
 
 
 class UserList(generics.ListCreateAPIView):
 
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+    queryset = models.User.objects.all()
+    serializer_class = serializers.UserSerializer
     permissions_classes = (IsAuthenticated, )
 
 
 class DeliveryList(generics.ListCreateAPIView):
 
-    queryset = Delivery.objects.all()
-    serializer_class = DeliverySerializer
+    queryset = models.Delivery.objects.all()
+    serializer_class = serializers.DeliverySerializer
+
+
+class OrderList(generics.ListCreateAPIView):
+
+    queryset = models.Order.objects.all()
+    serializer_class = serializers.OrderSerializer
+    permissions_classes = (IsAuthenticated, )
+
+
+class PaymentList(generics.ListCreateAPIView)::
+
+    queryset = models.Payment.objects.all()
+    serializer_class = serializers.PaymentSerializer
+
+
+class TripList(generics.ListCreateAPIView):
+
+    queryset = models.Trip.objects.all()
+    serializer_class = serializers.TripSerializer
+
+
+class RatingList(generics.ListCreateAPIView):
+
+    queryset = models.Rating.objects.all()
+    serializer_class = serializers.Rating
